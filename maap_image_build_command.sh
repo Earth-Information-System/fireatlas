5f7fd35ca5ab4c9c3d6550d71604e81bce9d9c6b
ranchodeluxe <greg.corradini@gmail.com>
2023-02-06 10:39:39 -0800

    try getting base image up

--- maap_image_build_command.sh
+++ maap_image_build_command.sh
@@ -4,7 +4,7 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P )
 mamba env create -f ${basedir}/env-feds.yaml
 pushd ${HOME}
 
-# Do not remove this (PMM Dec 2022)
-/opt/conda/envs/icesat2_boreal/bin/pip install --user -e git+https://github.com/MAAP-Project/maap-py.git#egg=maappy
+# required to get all the MAAP(y) things
+/opt/conda/envs/feds/bin/pip install --user -e git+https://github.com/MAAP-Project/maap-py.git#egg=maappy
 
-source activate base
+source activate feds
