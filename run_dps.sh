e362f5e917f21ad14b0f23813ea76e55fa68b3b0
ranchodeluxe <greg.corradini@gmail.com>
2023-02-06 16:15:51 -0800

    stuff

--- run_dps.sh
+++ run_dps.sh
@@ -5,27 +5,28 @@ set -eo pipefail
 basedir=$( cd "$(dirname "$0")"; pwd -P )
 echo "Basedir: $basedir"
 echo "Initial working directory: $(pwd -P)"
+
 echo "conda: $(which conda)"
 
 # Only create conda environment if it's not currently active. This allows for
 # interactive testing of this script.
-#if [[ $CONDA_PREFIX != "/projects/env-feds" ]]; then
-#  # Trying to resolve conda permissiosn issue
-#  # https://gitter.im/conda/conda?at=5dc427aa2f8a034357513172
-#  export CONDA_PKGS_DIRS="$basedir/.conda"
-#  mkdir -p "$CONDA_PKGS_DIRS"
-#  conda env create -f "$basedir/env-feds.yml" -p "$basedir/env-feds"
-#  source activate "$basedir/env-feds"
-#fi
-source activate feds
+if [[ $CONDA_PREFIX != "/projects/env-feds" ]]; then
+  # Trying to resolve conda permissiosn issue
+  # https://gitter.im/conda/conda?at=5dc427aa2f8a034357513172
+  export CONDA_PKGS_DIRS="$basedir/.conda"
+  mkdir -p "$CONDA_PKGS_DIRS"
+  conda env create -f "$basedir/env-feds.yml" -p "$basedir/env-feds"
+  source activate "$basedir/env-feds"
+fi
+
 echo "Python: $(which python)"
 python --version
+
 echo "Starting algorithm in subshell"
 (
 cd "$basedir"
 echo "Running in directory: $(pwd -P)"
 #python -u -c "import FireRun; FireRun.CreekSamplerun()"
-#python FireRun.py
 python FireRun.py
 )
 echo "Done!"
