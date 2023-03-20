b831a8be49060a6bf61fc958de6d1464c34d8c02
ksharonin <ksharonin@berkeley.edu>
2023-03-20 06:06:49 +0000

    add log to run

--- run_micro_dps.sh
+++ run_micro_dps.sh
@@ -9,20 +9,31 @@ echo "Initial working directory: $(pwd -P)"
 
 # install micromamba
 curl micro.mamba.pm/install.sh | bash
-# set variables
-# MAMBA_EXE="$basedir/bin/micromamba" # path to micromamba binary
-# ASSUME MAMBA_EXE GETS SET BY PROGRAM AS A DEFAULT ENV VAR
 echo $MAMBA_EXE
-MY_MAMBA_ENV="$basedir/micromamba/envs/rio-tiler-new"
+# set variables
+export MAMBA_EXE="$basedir/micromamba" # path to micromamba binary
+export MY_MAMBA_ENV="$basedir/micromamba/envs/rio-tiler-new"
 echo $PATH
 echo "micromamba version: $($MAMBA_EXE --version)"
 
 # flag - may need to modify order of arguments
 "$MAMBA_EXE" create -f "$basedir/rio-tiler-new.yml" -p "$MY_MAMBA_ENV"
 
-# creek sample run with exec
-"$MAMBA_EXE" run -p "$MY_MAMBA_ENV" python -u -c "import FireRun; FireRun.CreekSamplerun()"
-
+echo "Starting algorithm in subshell"
+(
+pushd "$basedir"
+{ # try
+  echo "Running in directory: $(pwd -P)"
+  "$MAMBA_EXE" run -p "$MY_MAMBA_ENV" python -u -c "import FireRun; FireRun.CreekSamplerun()"
+  popd
+  echo "Copying log to special output dir"
+  cp "$basedir/running.log" ./output
+} || { # catch
+  popd
+  echo "Copying log to special output dir"
+  cp "$basedir/running.log" ./output
+}
+)
 echo "Done!"
 
 exit
