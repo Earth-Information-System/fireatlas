65c83cf62d54414529055a67c0e418bdceedba93
ksharonin <ksharonin@berkeley.edu>
2023-03-20 04:14:49 +0000

    changed create line to fit previous syntax

--- run_micro_dps.sh
+++ run_micro_dps.sh
@@ -14,7 +14,7 @@ MAMBA_EXE="$basedir/micromamba" # path to micromamba binary
 MY_MAMBA_ENV="$basedir/micromamba/envs/rio-tiler-new"
 
 # flag - may need to modify order of arguments
-"$MAMBA_EXE" create -p "$MY_MAMBA_ENV" "$basedir/rio-tiler-new.yml"
+"$MAMBA_EXE" create -f "$basedir/rio-tiler-new.yml" -p "$MY_MAMBA_ENV"
 
 # creek sample run with exec
 "$MAMBA_EXE" run -p "$MY_MAMBA_ENV" python -u -c "import FireRun; FireRun.CreekSamplerun()"
