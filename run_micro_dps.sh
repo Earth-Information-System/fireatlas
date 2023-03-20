e8bf64166ce59bf10536708037512564f97bc7e6
ksharonin <ksharonin@berkeley.edu>
2023-03-20 16:46:58 +0000

    manual micromamba installation

--- run_micro_dps.sh
+++ run_micro_dps.sh
@@ -7,24 +7,34 @@ basedir=$( cd "$(dirname "$0")"; pwd -P )
 echo "Basedir: $basedir"
 echo "Initial working directory: $(pwd -P)"
 
-# install micromamba
-curl micro.mamba.pm/install.sh | bash
-echo $MAMBA_EXE
-# set variables
-export MAMBA_EXE="$basedir/micromamba" # path to micromamba binary
-export MY_MAMBA_ENV="$basedir/micromamba/envs/rio-tiler-new"
+# automatic install micromamba
+# curl micro.mamba.pm/install.sh | bash
+
+# micromamba binary dir
+MICROMAMBA_BIN_DIR="$basedir/bin"
+# create that dir if it doesn't exist
+mkdir -p "$MICROMAMBA_BIN_DIR"
+# set mambda micro exe 
+MICROMAMBA_EXE="$MICROMAMBA_BIN_DIR/micromamba"
+echo $MICROMAMBA_EXE
+# Download and extract binary to that location
+curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj "$MICROMAMBA_EXE"
+
+MY_MAMBA_ENV="$basedir/micromamba/envs/rio-tiler-new"
+# MY_MAMBA_ENV="$MICROMAMBA_BIN_DIR/envs/rio-tiler-new"
+
 echo $PATH
-echo "micromamba version: $($MAMBA_EXE --version)"
+echo "micromamba version: $($MICROMAMBA_EXE --version)"
 
 # flag - may need to modify order of arguments
-"$MAMBA_EXE" create -f "$basedir/rio-tiler-new.yml" -p "$MY_MAMBA_ENV"
+"$MICROMAMBA_EXE" create -f "$basedir/rio-tiler-new.yml" -p "$MY_MAMBA_ENV"
 
 echo "Starting algorithm in subshell"
 (
 pushd "$basedir"
 { # try
   echo "Running in directory: $(pwd -P)"
-  "$MAMBA_EXE" run -p "$MY_MAMBA_ENV" python -u -c "import FireRun; FireRun.CreekSamplerun()"
+  "$MICROMAMBA_EXE" run -p "$MY_MAMBA_ENV" python -u -c "import FireRun; FireRun.CreekSamplerun()"
   popd
   echo "Copying log to special output dir"
   cp "$basedir/running.log" ./output
