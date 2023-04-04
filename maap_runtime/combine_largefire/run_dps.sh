ca79b6e911d35f0b6df1cd9c5ca91893b066a1cf
gsfc_landslides <eorland_gee@gmail.com>
2023-04-04 16:51:19 +0000

    register new alg: combine_largefire

---
+++ maap_runtime/combine_largefire/run_dps.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+mkdir output
+
+# set -euxo pipefail
+set -eo pipefail
+
+basedir=$( cd "$(dirname "$0")"; pwd -P )
+echo "Basedir: $basedir"
+echo "Initial working directory: $(pwd -P)"
+
+echo "conda: $(which conda)"
+
+# Only create conda environment if it's not currently active. This allows for
+# interactive testing of this script.
+if [[ $CONDA_PREFIX != "/projects/env-feds" ]]; then
+  # Trying to resolve conda permissiosn issue
+  # https://gitter.im/conda/conda?at=5dc427aa2f8a034357513172
+  export CONDA_PKGS_DIRS="$basedir/.conda"
+  mkdir -p "$CONDA_PKGS_DIRS"
+  conda env create -f "$basedir/env-feds.yml" -p "$basedir/env-feds"
+  source activate "$basedir/env-feds"
+fi
+
+echo "Python: $(which python)"
+python --version
+
+echo "Starting algorithm in subshell"
+(
+pushd "$basedir"
+{ # try
+  echo "Running in directory: $(pwd -P)"
+  python combine_largefire.py
+  popd
+  echo "Copying log to special output dir"
+  cp "$basedir/running.log" ./output
+} || { # catch
+  popd
+  echo "Copying log to special output dir"
+  cp "$basedir/running.log" ./output
+}
+)
+echo "Done!"
+
+exit
