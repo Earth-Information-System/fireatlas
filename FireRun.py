dacf96d7b6bae6c39d853c6cfd3359c2e01f629e
gsfc_landslides <eorland_gee@gmail.com>
2023-04-12 17:18:29 +0000

    typo fix in consts file

--- FireRun.py
+++ FireRun.py
@@ -566,13 +566,13 @@ def MidWest_LF_Pix_Test():
         ampm = 'PM'
     else:
         ampm = 'AM'
-    tst = [2023, 3, 26, 'AM']
+    tst = [2023, 4, 5, 'AM']
     #ted = [2022, 12, 31, 'PM']
     ted = [ctime.year, ctime.month, ctime.day, ampm]
     print(f"Running code from {tst} to {ted} with source {FireConsts.firesrc}")
 
-    FireMain.Fire_Forward(tst=tst, ted=ted, restart=False, region=region)
-    FireGpkg.save_gdf_trng(tst=tst, ted=ted, regnm=region[0])
+    #FireMain.Fire_Forward(tst=tst, ted=ted, restart=False, region=region)
+    #FireGpkg.save_gdf_trng(tst=tst, ted=ted, regnm=region[0])
     FireGpkg_sfs.save_sfts_trng(tst, ted, regnm=region[0])
     tend = time.time()
     logger.info(f"{(tend-tstart)/60.} minutes used for {region[0]} with dask.")
