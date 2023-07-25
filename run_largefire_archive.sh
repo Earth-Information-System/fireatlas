42e1530aa850aec87fd40ec2228f9487ce33bb08
ranchodeluxe <greg.corradini@gmail.com>
2023-07-25 16:23:54 -0700

    updates for new ade and dps image

--- run_largefire_archive.sh
+++ run_largefire_archive.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
-mkdir output
-
 # set -euxo pipefail
 set -eo pipefail
-
+mkdir output
 basedir=$( cd "$(dirname "$0")"; pwd -P )
 echo "Basedir: $basedir"
 echo "Initial working directory: $(pwd -P)"
-
 echo "conda: $(which conda)"
-
-# Only create conda environment if it's not currently active. This allows for
-# interactive testing of this script.
-if [[ $CONDA_PREFIX != "/projects/env-feds" ]]; then
-  # Trying to resolve conda permissiosn issue
-  # https://gitter.im/conda/conda?at=5dc427aa2f8a034357513172
-  export CONDA_PKGS_DIRS="$basedir/.conda"
-  mkdir -p "$CONDA_PKGS_DIRS"
-  conda env create -f "$basedir/env-feds.yml" -p "$basedir/env-feds"
-  source activate "$basedir/env-feds"
-fi
-
 echo "Python: $(which python)"
 python --version
-
 echo "Starting algorithm in subshell"
 (
 pushd "$basedir"
