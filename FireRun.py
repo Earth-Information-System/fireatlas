b3c6e0af12774fec73159b5894b4af3603e3d8d4
tmccabe <tmccabe@bu.edu>
2023-10-12 07:58:04 -0700

    Adding an auto-date to borealNA

--- FireRun.py
+++ FireRun.py
@@ -235,8 +235,7 @@ def BorealNA():
     logger.info(f'STARTING RUN FOR {region[0]}')
     tstart = time.time()
     
-    #lts = FireIO.get_lts_serialization(regnm=region[0])
-    lts = None
+    lts = FireIO.get_lts_largefire(regnm=region[0])
     if lts == None:
         tst = [ctime.year, 1, 1, 'AM']
     else:
