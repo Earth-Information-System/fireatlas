3ddb57a22332c331f35f4b426331dc57aa178442
gsfc_landslides <eorland.gee@gmail.com>
2023-10-23 18:36:06 +0000

    added sumatra functionality

--- FireRun.py
+++ FireRun.py
@@ -308,19 +308,20 @@ def ItalyGreeceNRT():
     
     tend = time.time()
     
-    logger.info(f"{(tend-tstart)/60.} minutes used for Italy and Greece for {tst} to {ted}.")    
+    logger.info(f"{(tend-tstart)/60.} minutes used for Italy and Greece for {tst} to {ted}.")
+    
 def EQAsiaNRT():
     import FireIO, FireMain, FireGpkg, FireGpkg_sfs
     import FireConsts
     from datetime import datetime
     import os
     
-    if FireConsts.epsg!=6933:
-        print('Please set the epsg code to 6933')
-        return
+    # if FireConsts.epsg!=6933:
+    #     print('Please set the epsg code to 6933')
+    #     return
     
     ctime = datetime.now()
-    region = ("SouthBorneo_NRT", [108.90191926582735, 
+    region = ("SouthBorneoSNPP_NRT", [108.90191926582735, 
                                   -4.330624162411689, 
                                   117.77887239082733,
                                   -0.7971300838360769])
@@ -339,9 +340,9 @@ def EQAsiaNRT():
         ampm = 'PM'
     else:
         ampm = 'AM'
-    tst = [2023,8,1,'AM']
+    #tst = [2023,8,1,'AM']
     ted = [ctime.year, ctime.month, ctime.day, ampm]
-    #ted = [2023,6,28,'AM']
+    #ted = [2023,10,10,'PM']
     print(f"Running code from {tst} to {ted}.")
     
     # do fire tracking
@@ -358,6 +359,53 @@ def EQAsiaNRT():
     logger.info(f"{(tend-tstart)/60.} minutes used for Eq Asia for {tst} to {ted}.") 
 
 
+def SumatraNRT():
+    import FireIO, FireMain, FireGpkg, FireGpkg_sfs
+    import FireConsts
+    from datetime import datetime
+    import os
+    
+    # if FireConsts.epsg!=6933:
+    #     print('Please set the epsg code to 6933')
+    #     return
+    
+    ctime = datetime.now()
+    region = ("SumatraSNPP_NRT", [102,-5,107,0])
+    
+    logger.info(f'STARTING RUN FOR {region[0]}')
+    tstart = time.time()
+    
+    lts = FireIO.get_lts_serialization(regnm=region[0])
+    if lts == None:
+        tst = [ctime.year, 1, 1, 'AM']
+    else:
+        #tst = FireObj.t_nb(lts, nb="previous") <-- this returns an error
+        tst = lts
+
+    if ctime.hour >= 18:
+        ampm = 'PM'
+    else:
+        ampm = 'AM'
+    
+    #tst = [2023,10,18,'AM']
+    ted = [ctime.year, ctime.month, ctime.day, ampm]
+    #ted = [2023,10,10,'PM']
+    print(f"Running code from {tst} to {ted}.")
+    
+    # do fire tracking
+    FireMain.Fire_Forward(tst=tst, ted=ted, restart=False, region=region)
+
+    # calculate and save snapshot files
+    FireGpkg.save_gdf_trng(tst=tst, ted=ted, regnm=region[0])
+
+    # calculate and save single fire files
+    FireGpkg_sfs.save_sfts_trng(tst, ted, regnm=region[0])
+    
+    tend = time.time()
+    
+    logger.info(f"{(tend-tstart)/60.} minutes used for Eq Asia for {tst} to {ted}.") 
+
+    
 def QuebecSampleRun():
     import FireIO, FireMain, FireGpkg, FireGpkg_sfs
     import FireConsts
