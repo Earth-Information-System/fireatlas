84be1b3125ea8f10a86fd202aeb5a608a1fe39ce
ranchodeluxe <greg.corradini@gmail.com>
2023-10-25 06:35:12 -0700

    wrap run_dps_v2.sh

--- run_dps_v2.sh
+++ run_dps_v2.sh
@@ -14,7 +14,8 @@ echo "Starting algorithm in subshell"
 pushd "$basedir"
 { # try
   echo "Running in directory: $(pwd -P)"
-  python FireRunByRegion.py $1 $2 $3 $4
+  # these need to be double quoted to pass along strings correctly formatted
+  python FireRunByRegion.py --regnm=$1 --bbox="$2" --tst="$3" --ted="$4"
   popd
   echo "Copying log to special output dir"
   cp "$basedir/running.log" ./output
