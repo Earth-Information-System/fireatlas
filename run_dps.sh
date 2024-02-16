ab1a4fffb1a76a51ab22a18ba48c54f3389db0fe
tmccabe <tmccabe@bu.edu>
2024-02-16 13:07:28 -0800

    oops, syntax error

--- run_dps.sh
+++ run_dps.sh
@@ -14,7 +14,7 @@ echo "Starting algorithm in subshell"
 pushd "$basedir"
 { # try
   echo "Running in directory: $(pwd -P)"
-  scalene --cli --no-browser --reduced-profile --html --column-width 180 --outfile ${PWD}/output/profile.html python FireRun.py $1
+  scalene --cli --no-browser --reduced-profile --html --column-width 180 --outfile ${PWD}/output/profile.html --- FireRun.py $1
   popd
   echo "Copying log to special output dir"
   cp "$basedir/running.log" ./output
