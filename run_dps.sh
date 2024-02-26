b343cb2e1be305b125939974c42699b687bd36ae
tmccabe <tmccabe@bu.edu>
2024-02-26 10:13:31 -0800

    fix on directory handeling

--- run_dps.sh
+++ run_dps.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 # set -euxo pipefail
 set -xeo pipefail
-mkdir output
+output_dir=${PWD}/output
+mkdir "${output_dir}"
 basedir=$( cd "$(dirname "$0")"; pwd -P )
 echo "Basedir: $basedir"
 echo "Initial working directory: $(pwd -P)"
 echo "conda: $(which conda)"
 echo "Python: $(which python)"
 python --version
-source activate /opt/conda/envs/env-feds-dask
-conda install -c conda-forge scalene
+conda_prefix=/opt/conda/envs/env-feds-dask
+source activate "${conda_prefix}"
+conda install -c conda-forge scalene -y -p "${conda_prefix}"
 echo "Starting algorithm in subshell"
 (
 pushd "$basedir"
 { # try
   echo "Running in directory: $(pwd -P)"
-  scalene --cli --no-browser --reduced-profile --html --column-width 180 --outfile output/profile.html --- FireRun.py $1
+scalene --cli --no-browser --reduced-profile --html --column-width 180 --outfile "${output_dir}/profile.html" --- FireRun.py $1
   popd
   echo "Copying log to special output dir"
   cp "$basedir/running.log" ./output
