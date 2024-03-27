741b65adbf17566c8a7b5681c68cf41090b28011
ranchodeluxe <gregcorradini@gmail.com>
2024-03-27 15:12:03 +0000

    update remove static sources

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,22 +16,27 @@ import pytest
     ],
 )
 def test_maybe_remove_static_sources(
+    monkeypatch,
     static_source_dir_fake,
     original_region: Region,
     should_remove_static_sources_bool: bool,
 ):
     # arrange
-    if not should_remove_static_sources_bool:
-        FireConsts.remove_static_sources_bool = False
+    if should_remove_static_sources_bool:
+        monkeypatch.setattr(FireConsts, 'remove_static_sources_bool', True)
     else:
-        FireConsts.remove_static_sources_bool = True
+        monkeypatch.setattr(FireConsts, 'remove_static_sources_bool', False)
 
     # act
     result_region = maybe_remove_static_sources(original_region, static_source_dir_fake)
 
     # assert
     if not should_remove_static_sources_bool:
-        assert result_region == original_region
+        assert result_region != original_region
+        name, geom = result_region
+        assert isinstance(geom, Polygon)
+        # make sure there are no interior rings to returned geometry
+        assert len(geom.interiors) == 0
     else:
         assert result_region != original_region
         name, geom = result_region
