942f7d248993e996019236d4dbb0e6c86c7d00b0
gcorradini <greg@developmentseed.org>
2024-03-29 17:54:19 -0700

    fix FireForward run

--- maap_runtime/run_fire_forward.sh
+++ maap_runtime/run_fire_forward.sh
@@ -16,7 +16,7 @@ pushd "$basedir"
   echo "Running in directory: $(pwd -P)"
   # python3 FireRunFireForward.py --regnm="CaliTestRun"  --tst="[2023,6,1,\"AM\"]" --ted="[2023,9,1,\"AM\"]"
   scalene --cli --no-browser --reduced-profile --html --column-width 180 \
-      --outfile "${output_dir}/profile.html" --- FireRunFireForward.py --regnm=$1 --tst="$3" --ted="$4"
+      --outfile "${output_dir}/profile.html" --- FireRunFireForward.py --regnm=$1 --tst="$2" --ted="$3"
   popd
   echo "Copying log to special output dir"
   cp "$basedir/running.log" "$output_dir"
