3abc175714e5a201ca74d5de286002f49cd9fc54
ranchodeluxe <greg.corradini@gmail.com>
2024-04-17 06:07:31 -0700

    so difficult

--- maap_runtime/FireRunDataUpdateChecker.py
+++ maap_runtime/FireRunDataUpdateChecker.py
@@ -17,9 +17,10 @@ def Run():
     :return: None
     """
     data_update_results = FireRunDaskCoordinator.job_data_update_checker()
+    data_update_results.compute()
 
     data_upload_results = [
-        FireRunDaskCoordinator.concurrent_copy_inputs_from_local_to_s3([data_update_results,], local_filepath)
+        FireRunDaskCoordinator.concurrent_copy_inputs_from_local_to_s3([None,], local_filepath)
         for local_filepath in glob.glob(f"{settings.LOCAL_PATH}/{settings.PREPROCESSED_DIR}/*/*.txt")
     ]
     dag = delayed(lambda x: x)(data_upload_results)
