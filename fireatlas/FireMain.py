7fd602d1205e80d9b27d979b030afb63683c315a
ranchodeluxe <greg.corradini@gmail.com>
2024-05-21 12:41:20 -0700

    logger.warn

--- fireatlas/FireMain.py
+++ fireatlas/FireMain.py
@@ -554,6 +554,11 @@ def Fire_Forward(tst: TimeStep, ted: TimeStep, restart=False, region=None, read_
         )
         if t_saved is None:
             logger.warn("No saved version of allfires and allpixels")
+        elif t_saved == ted:
+            logger.warn(
+                "Nothing left to do. There is already a saved version "
+                f"of allfires and allpixels at {t_saved=}. Reprocessing.."
+            )
     if t_saved:
         # list of all the timesteps that we still need to run on
         list_of_ts = list(FireTime.t_generator(FireTime.t_nb(t_saved, "next"), ted))
