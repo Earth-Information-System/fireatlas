24ba01367475e42c0e0d149eceb505d2b7a28152
ranchodeluxe <greg.corradini@gmail.com>
2024-06-04 12:52:13 -0700

    back to 3.1.4

--- maap_runtime/run_dps_build.sh
+++ maap_runtime/run_dps_build.sh
@@ -10,6 +10,9 @@ python --version
 # where mamba should be default resolver
 pushd "$basedir"
 source activate /opt/conda/envs/vanilla
+# vanilla:3.1.5 makes all our code just hang for reasons know one knows about
+# so we explicitly upgrade just s3fs here out-of-band of pyproject.toml and env.yml
+conda update -c conda-forge s3fs -y
 /opt/conda/envs/vanilla/bin/pip install -e ..
 /opt/conda/envs/vanilla/bin/pip install "git+https://github.com/MAAP-Project/maap-py.git@develop"
 
