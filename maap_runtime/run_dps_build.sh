824b474b219eb7d1d74daf57319a450bb48149f6
ranchodeluxe <greg.corradini@gmail.com>
2024-06-04 20:25:19 -0700

    try it again

--- maap_runtime/run_dps_build.sh
+++ maap_runtime/run_dps_build.sh
@@ -12,7 +12,10 @@ pushd "$basedir"
 source activate /opt/conda/envs/vanilla
 # vanilla:3.1.5 makes all our code just hang for reasons know one knows about
 # so we explicitly upgrade just s3fs here out-of-band of pyproject.toml and env.yml
-conda update -c conda-forge s3fs -y
+conda update -c conda-forge s3fs -y > update_log.txt 2>&1
+cat update_log.txt
+conda list | grep s3fs
+echo "Doing fireatlas install next..."
 /opt/conda/envs/vanilla/bin/pip install -e ..
 /opt/conda/envs/vanilla/bin/pip install "git+https://github.com/MAAP-Project/maap-py.git@develop"
 
