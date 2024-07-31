c64ce2ec113c923e2832b4f61aed9f86672c5e6f
ranchodeluxe <greg.corradini@gmail.com>
2024-07-31 15:51:49 -0700

    alert GH action

---
+++ maap_runtime/alert-on-failed-dps-jobs.py
@@ -0,0 +1,71 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+from datetime import datetime, timezone, timedelta
+from maap.maap import MAAP
+maap = MAAP(maap_host='api.maap-project.org')
+
+now_utc = datetime.now(timezone.utc)
+
+
+def send_email(job_info: list):
+
+    if not job_info:
+        return
+
+    sender_email = "fireatlatalerts@gmail.com"
+    password = os.environ['EMAIL_PW']
+    receiver_emails = "gregcorradini@gmail.com, tempest.mccabe@nasa.gov, elijah.orland@nasa.gov"
+
+    subject = f"Job Failures Since {now_utc-timedelta(hours=1)}"
+
+    body = []
+    for job in job_info:
+        job_id, cmd, tag = job
+        body.append(f"{job_id} running command='{cmd}' with tag='{tag}' failed")
+    body = body.join("\n")
+
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_emails
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_emails.split(","), message.as_string())
+        print("Email sent successfully")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        server.quit()
+
+
+def filter_jobs_last_hour(jobs, status):
+    filtered_jobs = []
+    for job_dict in jobs:
+        # ignore key and get the value
+        job_value_dict = list(job_dict.values())[0]
+        if job_value_dict['status'] == status:
+            time_end_iso = job_value_dict['job']['job_info']['time_end']
+            time_end_dt = datetime.fromisoformat(time_end_iso.replace('Z', '+00:00'))
+            time_end_dt = time_end_dt.astimezone(timezone.utc)
+            time_difference = abs(now_utc - time_end_dt)
+            if time_difference <= timedelta(hours=1):
+                filtered_jobs.append([job_value_dict['payload_id'], job_value_dict['job']['params']['_command'], job_value_dict['job']['tag']])
+    return sorted(filtered_jobs, key=lambda l: l[0])
+
+
+def list_jobs():
+    jobs = maap.listJobs(username='gcorradini')
+    return jobs.json()
+
+
+if __name__ == '__main__':
+    jobs = list_jobs()
+    failed_jobs = filter_jobs_last_hour(jobs['jobs'], 'job-failed')
+    send_email(failed_jobs)
+
