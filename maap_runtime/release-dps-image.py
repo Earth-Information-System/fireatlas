b2ce1630aa561b4aa8b4d777f6a3babb203188e8
ranchodeluxe <greg.corradini@gmail.com>
2024-07-31 12:12:14 -0700

    try this

--- maap_runtime/release-dps-image.py
+++ maap_runtime/release-dps-image.py
@@ -3,6 +3,6 @@ import os
 
 maap = MAAP(maap_host='api.maap-project.org')
 current_dir = os.getcwd()
-target_config = f'{current_dir}/coordinator/algorithm_config.yaml'
+target_config = f'{current_dir}/maap_runtime/coordinator/algorithm_config.yaml'
 response = maap.register_algorithm_from_yaml_file(target_config)
 print(response.text)
