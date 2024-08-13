f8815a95541dea319689fa43da635bf611e6caba
Lisa M Rosenthal <lisa.rosenthal@arb.ca.gov>
2024-08-13 13:48:39 -0700

    made logger from a class in order to redefine file handler path

--- fireatlas/FireLog.py
+++ fireatlas/FireLog.py
@@ -1,41 +1,59 @@
 import logging
 import os
 from fireatlas import settings
+from fireatlas.FireConsts import root_dir
 
-_logger_configured = False
+# singleton ensures logger is initiated only once
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+    
+class CustomLogger(logging.Logger, metaclass=Singleton):
+    def __init__(self, name):
+        super().__init__(name)
+        self.default_file_path = os.path.join(root_dir, settings.LOG_FILENAME)
+        self.configure_logger()
 
-def get_logger(name):
-    from fireatlas.FireConsts import root_dir
+    def create_handler(self, handler, level):
+        handler.setLevel(level)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.addHandler(handler)
 
-    global _logger_configured
+    def set_fh(self, path):
+        self.fh = logging.FileHandler(path)
+        self.create_handler(self.fh, logging.INFO)
 
-    logger = logging.getLogger(name)
-
-    if not _logger_configured:
-        logger.setLevel(logging.INFO)
+    def configure_logger(self):
+        self.setLevel(logging.INFO)
 
         # create a console handler and set its level
         ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
+        self.create_handler(ch, logging.INFO)
 
         # create a file handler as well
-        fh = logging.FileHandler(os.path.join(root_dir, settings.LOG_FILENAME))
-        fh.setLevel(logging.INFO)
-
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        fh.setFormatter(formatter)
-
-        logger.addHandler(ch)
-        logger.addHandler(fh)
+        self.set_fh(self.default_file_path)
 
         # To avoid duplicate log messages when using `getLogger` with the same name,
         # prevent further propagation of messages to the root logger
-        logger.propagate = False
+        self.propagate = False # not sure if this does anything actually
+        self.info("logger initialized!")
 
-        logger.info("logger initialized!")
-        _logger_configured = True
+    def update_fh_dir(self, dirpath):
+        self.removeHandler(self.fh)
+        newpath = self.default_file_path.replace(root_dir, dirpath)
+        os.makedirs(dirpath, exist_ok=True)
+        self.set_fh(newpath)
+    
+    def reset_fh(self):
+        self.update_fh_dir(os.path.dirname(self.default_file_path)) 
 
-    return logger
 
-logger = get_logger(__name__)
+# Register the custom logger class with the logging module
+logging.setLoggerClass(CustomLogger)
+
+# create logger instance
+logger = logging.getLogger(__name__)      
