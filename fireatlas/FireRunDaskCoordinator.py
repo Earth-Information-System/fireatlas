2bb36d022ae5b0efe857fe3f5fad82899094e23b
ranchodeluxe <greg.corradini@gmail.com>
2024-08-12 18:32:32 -0700

    moar

--- fireatlas/FireRunDaskCoordinator.py
+++ fireatlas/FireRunDaskCoordinator.py
@@ -72,8 +72,10 @@ def get_timesteps_needing_region_t_processing(
         # for NRT make sure the current two day window
         # is constantly being refreshed to incorporate batch updates
         # and ignore if a couple extra timesteps end up duplicated in processing
-        tst = date(*ted[:-1]) - timedelta(days=1)
-        if date(*tst[:-1]) < date(*ted[:-1]): needs_processing.extend([t for t in t_generator(tst, ted)])
+        tst = t_nb(t_nb(ted, nb="previous"), nb="previous")
+        dtst, dted = date(*tst[:-1]), date(*ted[:-1])
+        if dtst < dted:
+            needs_processing.extend([t for t in t_generator(tst, ted)])
     return needs_processing
 
 
