b2d0a2244791b8152772b814a2c23fb78e758bb3
Lisa M Rosenthal <lisa.rosenthal@arb.ca.gov>
2024-08-14 18:06:09 -0700

    expanding FireLog with the functions approach

--- fireatlas/FireLog.py
+++ fireatlas/FireLog.py
@@ -2,58 +2,86 @@ import logging
 import os
 from fireatlas import settings
 from fireatlas.FireConsts import root_dir
+from functools import wraps 
 
-# singleton ensures logger is initiated only once
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-    
-class CustomLogger(logging.Logger, metaclass=Singleton):
-    def __init__(self, name):
-        super().__init__(name)
-        self.default_file_path = os.path.join(root_dir, settings.LOG_FILENAME)
-        self.configure_logger()
+DEFAULT_FILE_PATH = os.path.join(root_dir, settings.LOG_FILENAME)
 
-    def create_handler(self, handler, level):
-        handler.setLevel(level)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.addHandler(handler)
+_logger_configured = False
 
-    def set_fh(self, path):
-        self.fh = logging.FileHandler(path)
-        self.create_handler(self.fh, logging.INFO)
+def create_handler(logger, handler):
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
-    def configure_logger(self):
-        self.setLevel(logging.INFO)
+def get_logger(name):
+
+    global _logger_configured
+
+    logger = logging.getLogger(name)
+
+    if not _logger_configured:
+        logger.setLevel(logging.INFO)
 
         # create a console handler and set its level
         ch = logging.StreamHandler()
-        self.create_handler(ch, logging.INFO)
+        create_handler(logger, ch)
 
         # create a file handler as well
-        self.set_fh(self.default_file_path)
+        fh = logging.FileHandler(DEFAULT_FILE_PATH)
+        create_handler(logger, fh)
+        
+        # add file handler as an attribute in order to possibly update
+        logger.fh = fh
 
         # To avoid duplicate log messages when using `getLogger` with the same name,
         # prevent further propagation of messages to the root logger
-        self.propagate = False # not sure if this does anything actually
-        self.info("logger initialized!")
+        logger.propagate = False
+        logger.info("logger initialized!")
 
-    def update_fh_dir(self, dirpath):
-        self.removeHandler(self.fh)
-        newpath = self.default_file_path.replace(root_dir, dirpath)
-        os.makedirs(dirpath, exist_ok=True)
-        self.set_fh(newpath)
-    
-    def reset_fh(self):
-        self.update_fh_dir(os.path.dirname(self.default_file_path)) 
+        _logger_configured = True
+
+    return logger
 
 
-# Register the custom logger class with the logging module
-logging.setLoggerClass(CustomLogger)
+def update_fh(logger, dirpath):
+    # remove the old one
+    logger.removeHandler(logger.fh)
+    newpath = DEFAULT_FILE_PATH.replace(os.path.dirname(DEFAULT_FILE_PATH), dirpath)
+    os.makedirs(dirpath, exist_ok=True)
 
-# create logger instance
-logger = logging.getLogger(__name__)      
+    # create new file handler
+    logger.fh = logging.FileHandler(newpath)
+    create_handler(logger, logger.fh)
+
+    return logger
+
+
+def reset_fh(logger):
+    return update_fh(logger, os.path.dirname(DEFAULT_FILE_PATH))
+
+# a decorator to apply to certain fuctions in order to change the file handler path
+def logger_subdir(all_dir_func, tst_pos, region_pos):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            # Extract tst and region from kwargs or args
+            tst = kwargs.get('tst', args[tst_pos] if len(args) > tst_pos else None)
+            region = kwargs.get('region', args[region_pos] if len(args) > region_pos else None)
+
+            # configure logger to put logs into subdirectory
+            if settings.LOG_SUBDIR and tst and region:
+                update_fh(logger, all_dir_func(tst, region, location = None))
+
+            result = f(*args, **kwargs)
+            
+                # reset logging path to default
+            if settings.LOG_SUBDIR and tst and region:
+                reset_fh(logger)
+                
+            return result
+        return wrapper
+    return decorator
+
+logger = get_logger(__name__)
