15e5c872a231296a5585f3cec44e892b0b17e4fe
zbecker <zebfullerbecker@gmail.com>
2025-09-22 15:58:15 -0700

    Update map key

--- fireatlas/DataCheckUpdate.py
+++ fireatlas/DataCheckUpdate.py
@@ -16,7 +16,7 @@ from fireatlas import settings
 from fireatlas.FireLog import logger
 from fireatlas.preprocess import preprocess_input_file
 
-MAP_KEY = "0e50658bd8e8ea368db7379b0be28630"
+MAP_KEY = "af58dc9df0eb9c54f4cad1e73c6eae4e"
 N_MAX_RETRIES = 30
 
 # ------------------------------------------------------------------------------
