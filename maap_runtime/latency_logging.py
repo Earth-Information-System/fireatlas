db1874740d1233ec8987f3982d9cd191f1bdd910
Tess McCabe <21266199+mccabete@users.noreply.github.com>
2026-03-09 12:04:50 -0400

    Update latency_logging.py
    12 hour latency expectation

--- maap_runtime/latency_logging.py
+++ maap_runtime/latency_logging.py
@@ -12,7 +12,7 @@ def fire_api_query(base = "https://openveda.cloud/api/features/collections/", co
     foo.raise_for_status()
 
 
-def get_time_difference_in_data(api_data, overpass_cadence = 12, baseline_latency = 10, some_time_buffer = 0, eastern_timezone_region = "US/Eastern"):
+def get_time_difference_in_data(api_data, overpass_cadence = 12, baseline_latency = 12, some_time_buffer = 0.20, eastern_timezone_region = "US/Eastern"):
   '''
   api_data (dict): json output from an api call to the FEDS api.
   overpass_cadence (float): expected number hours between data collections
