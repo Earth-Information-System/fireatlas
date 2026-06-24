5f96e6ba0a25866a149caf2e5b1a50175d9cf314
Zeb Becker <zebfullerbecker@gmail.com>
2026-06-24 11:29:17 -0700

    Bump chained job version

--- maap_runtime/FireRunArchiveCoordinator.py
+++ maap_runtime/FireRunArchiveCoordinator.py
@@ -164,12 +164,12 @@ def main(run_name, copy_to_veda=False):
         
         maap = MAAP(maap_host='api.maap-project.org')
         job = maap.submitJob(
-            identifier=f"job-eis-feds-archive:1.4.1",
-        algo_id="eis-feds-archive",
-        version="1.4.1",
-        username="zbecker", 
-        queue="maap-dps-eis-worker-128gb",
-        run_id=run_name
+            identifier=f"job-eis-feds-archive:1.5.1",
+            algo_id="eis-feds-archive",
+            version="1.5.1",
+            username="zbecker", 
+            queue="maap-dps-eis-worker-128gb",
+            run_id=run_name
         )
 
         logger.info(f"------------- Submitted next job to DPS. Submission status: {job.status} -------------")
