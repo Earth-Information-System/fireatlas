5ebd7c6d6e534069f2feee1863d07e3224153117
Zeb Becker <zebfullerbecker@gmail.com>
2026-07-15 10:38:00 -0700

    bump version number

--- maap_runtime/FireRunArchiveCoordinator.py
+++ maap_runtime/FireRunArchiveCoordinator.py
@@ -164,9 +164,9 @@ def main(run_name, copy_to_veda=False):
         
         maap = MAAP(maap_host='api.maap-project.org')
         job = maap.submitJob(
-            identifier=f"job-eis-feds-archive:1.5.2",
+            identifier=f"job-eis-feds-archive:1.5.3",
             algo_id="eis-feds-archive",
-            version="1.5.2",
+            version="1.5.3",
             username="zbecker", 
             queue="maap-dps-eis-worker-128gb",
             run_id=run_name
